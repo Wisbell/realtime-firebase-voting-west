cc82ba098e62baac903d18e3d17fc6931449c6d2
Wisbell <westleyisbell@gmail.com>
2017-01-31 13:37:26 -0600

    Added scotts code to mine to fix my errors

--- main.js
+++ main.js
@@ -7,44 +7,33 @@
       storageBucket: "west-voting-football.appspot.com",
       messagingSenderId: "71850084430"
     };
+
     firebase.initializeApp(config);
 
-  document
+ document
     .querySelectorAll('.choice button')
     .forEach(btn => btn.addEventListener('click', onVote))
 
+  function onVote (evt) {
+    // submit the vote
+    const voteURL = 'https://west-voting-football.firebaseio.com/votes.json'
 
-  function onVote(event){
-    console.log("onVote button clicked")
-
-    //submit the vote
     // // what button i clicked on
-    // console.log(event.target.dataset.value)
-    const voteFor = event.target.closest('.choice').dataset.value
-    const url = 'https://west-voting-football.firebaseio.com/votes.json'
+    const voteFor = evt.target.closest('.choice').dataset.value
 
-
-    firebase.database().ref('votes').once('value', snap => snap.val() )
-
+    // // go get the current counts
+    firebase.database().ref('votes').once('value')
+      .then(snap => snap.val())
       .then(data => {
-        // patch new count
-
-        const newCount = data && data[voteFor] ? data[voteFor] + 1 : 1
-
-        return firebase.database().ref('votes').update({[voteFor]: newCount})
+        // patch the new count
+        const newCount = data && data[voteFor] ? data[voteFor] += 1 : 1
+        return firebase.database().ref('votes').update({ [voteFor]: newCount })
       })
-
       .catch(console.error)
-
-
-
-    // hide buttons
 
     document.querySelectorAll('button').forEach(btn => btn.remove())
     document.querySelectorAll('.hidden').forEach(item => item.classList.remove('hidden'))
-
-  } // clone onVote function
-
+  }
 
   firebase.database().ref('votes').on('value', onUpdate)
 
@@ -52,16 +41,12 @@
     const data = snap.val()
 
     document.querySelectorAll('h3').forEach(choice => {
-            // reduce first arg - previous value - second arg next value
-            const total = Object.values(data).reduce((acc, val) => acc + val)
-            const current = data[choice.closest('.choice').dataset.value]
-            choice.innerText = Math.round(current / total * 100) + "%"
-          })
+      const total = Object.values(data).reduce((acc, val) => acc + val)
+      const current = data[choice.closest('.choice').dataset.value]
+      choice.innerText = Math.round(current / total * 100) + "%"
+    })
   }
-
-
-
-} // Close script in brackets to make es6 variables non global
+}
 
 
 
